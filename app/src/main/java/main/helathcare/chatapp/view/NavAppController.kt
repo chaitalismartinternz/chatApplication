0a6aa289cff73ba406e933b481c9867b1030ba4b
Bhagyashri Lokhande <bhagyashri.lokhande590@gmail.com>
2024-11-07 11:32:34 +0530

    chat app initial committe

--- app/src/main/java/main/helathcare/chatapp/view/NavAppController.kt
+++ app/src/main/java/main/helathcare/chatapp/view/NavAppController.kt
@@ -16,6 +16,7 @@ class NavAppController : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            bdbd
             ChatAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
